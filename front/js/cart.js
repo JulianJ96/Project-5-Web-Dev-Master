fcbec658774b8b78696d9153f74b5e7913f31d4b
Julian Jones <julianj96@yahoo.com>
2022-11-16 15:02:08 -0600

    setting up cart js

---
+++ front/js/cart.js
@@ -0,0 +1,7 @@
+
+
+
+
+const displayItems = document.getElementById('cart__items');
+const qtyDisplay = document.getElementById('totalQuantity');
+const totalPrice = document.getElementById('totalPrice');
